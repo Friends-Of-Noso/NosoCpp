3652666dea4f9151ed502f2f057b7e9fffd8ffc9
Gustavo Carreno <guscarreno@gmail.com>
2023-09-12 06:28:43 +0100

    chore: Starting this ordeal all over again

---
+++ build_with_cmake_vcpkg.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+GENERATE_PRESET=ninja-multi-vcpkg
+BUILD_PRESET=ninja-vcpkg-release
+#PACKAGE_PRESET_ZIP=package-zip-release
+#PACKAGE_PRESET_TGZ=package-tar-gz-release
+PACKAGE_PRESET_DEB=package-deb-release
+#PACKAGE_PRESET_RPM=package-rpm-release
+#PACKAGE_PRESET_NSIS64=package-nsis64-release
+BUILDS=builds/ninja-multi-vcpkg
+
+if [ "$1" == "--clean" ]
+then
+  echo "#####"
+  echo "# Cleaning builds folder"
+  echo "#####"
+  if [ -d ${BUILDS} ]
+  then
+    rm -r ${BUILDS}
+  else
+    echo
+    echo "Nothing to clean"
+  fi
+fi
+
+echo "#####"
+echo "# Creating CMake build files"
+echo "#####"
+cmake --preset ${GENERATE_PRESET}
+
+if [ $? == 0 ]
+then
+  echo
+  echo "#####"
+  echo "# Building NosoCpp"
+  echo "#####"
+  cmake --build --preset ${BUILD_PRESET}
+fi
+
+#if [ $? == 0 ]
+#then
+#  echo
+#  echo "#####"
+#  echo "# Packaging NosoCpp *.zip"
+#  echo "#####"
+#  cpack --preset ${PACKAGE_PRESET_ZIP}
+#fi
+
+#if [ $? == 0 ]
+#then
+#  echo
+#  echo "#####"
+#  echo "# Packaging NosoCpp *.tar.gz"
+#  echo "#####"
+#  cpack --preset ${PACKAGE_PRESET_TGZ}
+#fi
+
+if [ $? == 0 ]
+then
+  echo
+  echo "#####"
+  echo "# Packaging NosoCpp *.deb"
+  echo "#####"
+  cpack --preset ${PACKAGE_PRESET_DEB}
+fi
+
+#if [ $? == 0 ]
+#then
+#  echo
+#  echo "#####"
+#  echo "# Packaging NosoCpp *.rpm"
+#  echo "#####"
+#  cpack --preset ${PACKAGE_PRESET_RPM}
+#fi
+
+#if [ $? == 0 ]
+#then
+#  echo
+#  echo "#####"
+#  echo "# Packaging NosoCpp install.exe"
+#  echo "#####"
+#  cpack --preset ${PACKAGE_PRESET_NSIS64}
+#fi
